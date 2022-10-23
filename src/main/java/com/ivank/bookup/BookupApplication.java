2f7cb1e44e7e251f3bb18e06d4709bf48e9f9df4
Ivan Krstev <ivan96krstev@gmail.com>
2022-10-23 14:28:16 +0200

    setup bookup spring-boot project

---
+++ src/main/java/com/ivank/bookup/BookupApplication.java
@@ -0,0 +1,13 @@
+package com.ivank.bookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivank/bookup/BookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivank.bookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
